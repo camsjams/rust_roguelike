306d20450a2ace9579e86183a5fcb35f7a871c3b
Cameron Manavian <cameron.manavian@gmail.com>
2018-09-19 13:44:40 -0700

    Fix traits

--- src/character.rs
+++ src/character.rs
@@ -1,59 +1,98 @@
 pub struct Character {
     pub name: String,
     pub class: String,
-    health: i32,
+    pub health: i32,
     attack: i32,
     dodge: i32,
     luck: i32,
+    xp: i32,
 }
 
 pub trait Player {
-    fn new(class_name: &str, health: i32, attack: i32, dodge: i32, luck: i32) -> Character;
+    fn new(
+        name: String,
+        class_name: String,
+        health: i32,
+        attack: i32,
+        dodge: i32,
+        luck: i32,
+    ) -> Character;
 
-    fn select(&mut self, player_name: &str, player_luck: i32);
+    fn select(&self, player_name: String, player_luck: i32) -> Self;
 
     fn damage(&mut self, damage_amount: i32);
-    
+
+    fn heal(&mut self, heal_amount: i32);
+
     fn attack(&self) -> i32;
 
     fn dodge(&self) -> i32;
 
     fn info(&self) -> String;
+
+    fn stats(&self) -> String;
 }
 
 impl Player for Character {
-    fn new(class_name: &str, health: i32, attack: i32, dodge: i32, luck: i32) -> Character {
+    fn new(
+        name: String,
+        class_name: String,
+        health: i32,
+        attack: i32,
+        dodge: i32,
+        luck: i32,
+    ) -> Character {
         Character {
-            name: "TBD".to_string(),
+            name: name.to_string(),
             class: class_name.to_string(),
             health: health,
             attack: attack,
             dodge: dodge,
             luck: luck,
+            xp: 0,
         }
     }
 
-    fn select(&mut self, player_name: &str, player_luck: i32) {
-        self.name = player_name.to_string();
-        self.luck += player_luck;
+    fn select(&self, player_name: String, player_luck: i32) -> Self {
+        Self::new(
+            player_name,
+            self.class.to_string(),
+            self.health,
+            self.attack,
+            self.dodge,
+            self.luck + player_luck,
+        )
     }
 
     fn damage(&mut self, damage_amount: i32) {
         self.health -= damage_amount;
+        self.xp += 2;
+    }
+
+    fn heal(&mut self, heal_amount: i32) {
+        self.health += heal_amount;
+        self.xp += 1;
     }
 
     fn attack(&self) -> i32 {
-        self.attack + self.luck / 2
+        self.xp + self.attack + self.luck / 2
     }
 
     fn dodge(&self) -> i32 {
-        self.dodge + self.luck / 2
+        self.xp + self.dodge + self.luck / 2
     }
 
     fn info(&self) -> String {
         format!(
-            "{}: hp: {} attack: {} dodge: {} luck: {}",
+            "{} \thp: {} attack: {} dodge: {} luck: {}",
             self.class, self.health, self.attack, self.dodge, self.luck
+        )
+    }
+
+    fn stats(&self) -> String {
+        format!(
+            "{} - hp: {} attack: {} dodge: {} luck: {} experience: {}",
+            self.class, self.health, self.attack, self.dodge, self.luck, self.xp
         )
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,8 @@ extern crate rand;
 extern crate text_io;
 
 mod character;
+
+use character::Player;
 use rand::Rng;
 use std::io;
 
@@ -13,15 +15,15 @@ fn main() {
         env!("CARGO_PKG_VERSION")
     );
 
-    let characters: Vec<&character::Player> = [
-        character:Character::new("Cleric", 7, 5, 6, 7),
-        character:Character::new("Warrior", 10, 5, 5, 5),
-        character:Character::new("Hunter", 5, 7, 7, 6),
-        character:Character::new("Wizard", 3, 10, 5, 7),
-        character:Character::new("Thief", 4, 5, 6, 10),
+    let characters: [character::Character; 5] = [
+        character::Character::new("".to_string(), "Cleric".to_string(), 7, 5, 6, 7),
+        character::Character::new("".to_string(), "Warrior".to_string(), 10, 5, 5, 5),
+        character::Character::new("".to_string(), "Hunter".to_string(), 5, 7, 7, 6),
+        character::Character::new("".to_string(), "Wizard".to_string(), 3, 10, 5, 7),
+        character::Character::new("".to_string(), "Thief".to_string(), 4, 5, 6, 10),
     ];
 
-    let luck_amount = rand::thread_rng().gen_range(2, 11);
+    let _luck_amount = rand::thread_rng().gen_range(2, 6);
 
     println!("You enter the Ephemeral Plane of Creation ...");
     println!("Please input your character name.");
@@ -31,21 +33,75 @@ fn main() {
     io::stdin()
         .read_line(&mut input_text)
         .expect("Failed to read line");
-    let character_name = input_text.trim();
+    let _character_name = input_text.trim();
 
     println!("Please select your character type:");
     for (i, elem) in characters.iter().enumerate() {
-        print!("\n{}. {}\n\n", i, elem.info());
+        print!("\n{}. {}\n\n", i + 1, elem.info());
     }
 
-    let mut character_index: usize = characters.len();
+    let mut character_index: usize = 100;
     while character_index > characters.len() {
         character_index = read!();
     }
 
-    play(characters[character_index]);
+    let mut player =
+        characters[character_index - 1].select(_character_name.to_string(), _luck_amount);
+
+    play(&mut player);
 }
 
-fn play(player: character::Character) {
-    println!("Welcome aboard {} the {}!", player.name, player.class);
+fn play(player: &mut character::Character) {
+    println!(
+        "=== Welcome to RRL {} the {}! ====\n",
+        player.name, player.class
+    );
+    println!("Your unique stats: {}", player.stats());
+    let mut _cpu_level = 1;
+    let mut _difficulty = 1;
+
+    while player.health > 0 {
+        println!(
+            "\n====\nYour health: {}\n(a)ttack or (d)odge\n",
+            player.health
+        );
+        let _cpu_action = rand::thread_rng().gen_range(_cpu_level, 16 + _difficulty);
+        _cpu_level += 1;
+        _difficulty += 3;
+
+        let mut input_text = String::new();
+
+        io::stdin()
+            .read_line(&mut input_text)
+            .expect("Failed to read line");
+        let _player_move = input_text.trim();
+
+        let mut _player_action = 0;
+
+        if _player_move == "a" {
+            _player_action = player.attack();
+            println!("> You attacked with {} attack power!", _player_action);
+            println!("> CPU dodged with {} dodge power!", _cpu_action);
+        } else {
+            _player_action = player.dodge();
+            println!("> CPU attacked with {} dodge power!", _cpu_action);
+            println!("> You dodged with {} dodge power!", _player_action);
+        }
+
+        if _player_action > _cpu_action {
+            let heal = (_player_action - _cpu_action) / 2;
+            println!("\nFlawless Victory (you heal {})", heal);
+            player.heal(heal)
+        } else {
+            let dmg = _cpu_action - _player_action;
+            println!("\nTis but a flesh wound (you take {} damage)", dmg);
+            player.damage(dmg)
+        }
+    }
+
+    println!(
+        "\n\n=== Game Over! ====\nYour soul has lost connection to this land.\n\nFinal stats:\n{}.\nCPU level: {}",
+        player.stats(),
+        _cpu_level
+    );
 }
